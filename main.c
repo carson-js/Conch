78877c72eedb2df4db9d63b01997a5455b4760af
Carson Smith <carsonjs@icloud.com>
2026-05-27 14:07:39 -0500

    completed tokenization into argv array, as well as EOF handling and removing extraneous \n

--- main.c
+++ main.c
@@ -2,14 +2,22 @@
 #include <string.h>
 
 int main(void){
-    char input[100];
     while(true){
+        char input[100];
+        char *argv[100];
+        int argc = 0;
         printf("conch> ");
-        fgets(input, sizeof(input), stdin);
+        if (fgets(input, sizeof(input), stdin) == NULL) break;
         char *token = strtok(input, " ");
-        while (token != NULL) {
-            printf("%s\n", token);
+        for (int i = 0; token != NULL; i++) {
+            if (*token == '\n') break;
+            if (token[strlen(token) - 1] == '\n') token[strlen(token) - 1] = '\0';
+            argv[i] = token;
+            ++argc;
             token = strtok(NULL, " ");
+        }
+        for (int i = 0; i<argc; i++) {
+            printf("%s\n", argv[i]);
         }
     }
     return 0;
