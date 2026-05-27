a380628f4f8ae37cb3fd78890b948901c1133cfd
Carson Smith <carsonjs@icloud.com>
2026-05-27 12:03:09 -0500

    added tokenization to input

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     char input[100];
     while(true){
         printf("conch> ");
-        scanf("%s", input); 
+        fgets(input, sizeof(input), stdin);
+        char *token = strtok(input, " ");
+        while (token != NULL) {
+            printf("%s\n", token);
+            token = strtok(NULL, " ");
+        }
     }
     return 0;
 }
